89131743929fe987252d42cf2898aa3d6b2fd462
Nicoomf <nicolas_medina28@yahoo.com>
2022-08-25 11:34:17 -0400

    Arreglado lo de actualizar eventos expirados en el SV

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -3,38 +3,40 @@ const helpers = {};
 const Event = require("../models/Eventos");
 
 helpers.isAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    req.flash('error_msg', 'No estas autorizado! Inicia sesión para continuar...');
-    res.redirect('/admin/signin');
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash(
+    "error_msg",
+    "No estas autorizado! Inicia sesión para continuar..."
+  );
+  res.redirect("/admin/signin");
 };
 
 // Updatea constantemente los eventos expirados...
 helpers.updateExpirados = async (req, res, next) => {
-    const eventI = await Event.find({ vigente: true });
-    console.log("entro!");
-    // console.log(eventI);
-    for (let index = 0; index < eventI.length; index++) {
-      var fechaHoy = new Date(
-        new Date().getFullYear(),
-        new Date().getMonth(),
-        new Date().getDate(),
-        00,
-        00
-      );
-      console.log(fechaHoy);
-  
-      if (fechaHoy >= eventI[index].fecha) {
-        await Event.findByIdAndUpdate(eventI[index]._id, {
-          vigente: false,
-        });
-        console.log("Updateado!");
-      }
-  
-    //   res.send("Updateado...");
+  const eventI = await Event.find({ vigente: true });
+  console.log("entro!");
+  // console.log(eventI);
+  for (let index = 0; index < eventI.length; index++) {
+    var fechaHoy = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      new Date().getDate(),
+      23,
+      59
+    );
+
+    if (fechaHoy >= eventI[index].fecha) {
+      await Event.findByIdAndUpdate(eventI[index]._id, {
+        vigente: false,
+      });
+      console.log("Updateado!");
     }
-    return next();
+    //   res.send("Updateado...");
   }
+  console.log(fechaHoy);
+  return next();
+};
 
 module.exports = helpers;
